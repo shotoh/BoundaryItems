5eafb8e31204e5d715e020e0d29d04b1a0c8a146
shotoh <trueshotoh@gmail.com>
2024-02-10 18:42:47 -0800

    added different paths for items

--- src/main/java/io/github/shotoh/boundaryitems/items/BoundaryItem.java
+++ src/main/java/io/github/shotoh/boundaryitems/items/BoundaryItem.java
@@ -1,7 +1,12 @@
 package io.github.shotoh.boundaryitems.items;
 
+import io.github.shotoh.boundaryitems.BoundaryItems;
 import net.minecraft.server.v1_8_R3.Material;
+import org.bukkit.inventory.ItemStack;
 
-public record BoundaryItem(String id, String name, String[] lore, Material material, ItemPath path,
-                           UpgradeInfo upgradeInfo, double itemStat) {
+public record BoundaryItem(String id, String name, String[] lore, Material material, double itemStat,
+                           ItemPath path, int pathWeight, UpgradeInfo upgradeInfo) {
+    public ItemStack create(BoundaryItems plugin) {
+        return null; // todo
+    }
 }
--- src/main/java/io/github/shotoh/boundaryitems/items/ItemManager.java
+++ src/main/java/io/github/shotoh/boundaryitems/items/ItemManager.java
@@ -1,10 +1,12 @@
 package io.github.shotoh.boundaryitems.items;
 
+import java.util.List;
 import java.util.Map;
 
 public class ItemManager {
     private static final ItemManager INSTANCE = new ItemManager();
     private Map<String, BoundaryItem> items;
+    private Map<ItemPath, List<String>> paths;
 
     private ItemManager() {
         // load items
