fc54e632b9c4a648ee9f480529b697cff30ff641
shotoh <trueshotoh@gmail.com>
2024-02-10 18:38:09 -0800

    registered vault integration

--- src/main/java/io/github/shotoh/boundaryitems/BoundaryItems.java
+++ src/main/java/io/github/shotoh/boundaryitems/BoundaryItems.java
@@ -1,6 +1,16 @@
 package io.github.shotoh.boundaryitems;
 
+import io.github.shotoh.boundaryitems.integrations.VaultIntegration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BoundaryItems extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        VaultIntegration.register(this);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
 }
